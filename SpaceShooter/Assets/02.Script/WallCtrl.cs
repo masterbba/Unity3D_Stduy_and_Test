8e947dc281b9d2b6f766cdb95e9e89b782c2c371
masterbba <masterbb@gmail.com>
2015-08-24 23:20:05 +0900

    Add Shot Effect, End Chapter 5

--- SpaceShooter/Assets/02.Script/WallCtrl.cs
+++ SpaceShooter/Assets/02.Script/WallCtrl.cs
@@ -3,10 +3,15 @@ using System.Collections;
 
 public class WallCtrl : MonoBehaviour
 {
+	public GameObject sparkEffect;
+
 	void OnCollisionEnter(Collision coll)
 	{
 		if (coll.collider.tag == "BULLET")
 		{
+			GameObject spark = (GameObject)Instantiate(sparkEffect, coll.transform.position, Quaternion.identity );
+			Destroy(spark, spark.GetComponent<ParticleSystem>().duration + 0.2f);
+
 			Destroy(coll.gameObject);
 		}
 	}
