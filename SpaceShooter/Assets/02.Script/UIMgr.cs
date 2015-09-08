5535bdc62920a877ce63b6ebcac1e27a8b650d2d
masterbba <masterbba@gmail.com>
2015-09-09 01:39:47 +0900

    UI base tutorial

---
+++ SpaceShooter/Assets/02.Script/UIMgr.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIMgr : MonoBehaviour
+{
+	public void OnClickStartBtn( RectTransform rt )
+	{
+		Debug.Log("Click Button "+rt.localScale.x.ToString());
+	}
+}
