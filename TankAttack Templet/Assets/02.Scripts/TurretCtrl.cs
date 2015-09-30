402e90905861c6c9a5a5b27a16b74b14e16ba3c3
masterbba <masterbba@gmail.com>
2015-10-01 02:40:29 +0900

    Add TankAttack

---
+++ TankAttack Templet/Assets/02.Scripts/TurretCtrl.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretCtrl : MonoBehaviour
+{
+    private Transform tr;
+    private RaycastHit hit;
+
+    public float rotSpeed = 5.0f;
+
+	void Start ()
+    {
+        tr = GetComponent<Transform>();
+	}
+
+	void Update ()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(ray.origin, ray.direction * 100.0f, Color.green);
+
+        if( Physics.Raycast(ray, out hit, Mathf.Infinity, 1<<8 ) )
+        {
+            Vector3 relative = tr.InverseTransformPoint(hit.point);
+            float angle = Mathf.Atan2(relative.x, relative.z) * Mathf.Rad2Deg;
+            tr.Rotate(0, angle * Time.deltaTime, 0);
+        }
+	}
+}
