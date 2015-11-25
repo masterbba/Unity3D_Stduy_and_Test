608bd7126c4864341431b7125357b959ba62d86d
masterbba <masterbba@gmail.com>
2015-11-25 19:01:35 +0900

    End Book

---
+++ FinalChapter/Assets/02.Scripts/RagdollCtrl.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RagdollCtrl : MonoBehaviour
+{
+    public Rigidbody[] rbody;
+    private Animation anim;
+    
+	void Start ()
+    {
+        anim = GetComponent<Animation>();
+        rbody = GetComponentsInChildren<Rigidbody>();
+        SetRagdoll(false);
+
+        StartCoroutine(this.WakeupRagdoll());
+	}
+
+    void SetRagdoll( bool isEnable )
+    {
+        foreach( Rigidbody _rbody in rbody )
+        {
+            _rbody.isKinematic = !isEnable;
+        }
+    }
+
+    IEnumerator WakeupRagdoll()
+    {
+        yield return new WaitForSeconds(3.0f);
+        anim.Stop();
+        SetRagdoll(true);
+    }
+}
